bc17f48e767849bd5ce22e7a6d564bcfd390003f
XenialDan <thebigsmilexd@gmail.com>
2022-01-14 23:12:26 +0100

    Initial working version with schematic 256x support

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+using VoxReader.Interfaces;
+using VoxToStructure.format.schematic;
+
+namespace VoxToStructure
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var formatItem = ComboBox.SelectedItem;
+            if (formatItem == null)
+            {
+                MessageBox.Show("Select an output format first!", "Output format", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var loader = new OpenFileDialog();
+            loader.Filter = "MagicaVoxel File (*.vox)|*.vox";
+            if (loader.ShowDialog() == true)
+                loadAndConvert(loader.FileName, ComboBox.Text);
+        }
+
+        private void loadAndConvert(string file, string targetFormat)
+        {
+            if (Path.GetExtension(file).Equals(".vox"))
+            {
+                IVoxFile vox = VoxReader.VoxReader.Read(file);
+                var outputPath = Path.ChangeExtension(file, targetFormat);
+                switch (targetFormat)
+                {
+                    case ".schematic":
+                    {
+                        var output = new SchematicFile();
+                        var schematicFile = output.FromVox(vox);
+                        output.Save(schematicFile, outputPath);
+                        MessageBox.Show("Conversion successful, saved to " + outputPath);
+                        break;
+                    }
+                    default:
+                    {
+                        MessageBox.Show("Format currently not supported", "Unsupported format", MessageBoxButton.OK, MessageBoxImage.Error);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Unrecognized File");
+            }
+        }
+    }
+}
---
+++ format/schematic/SchematicFile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using fNbt;
+using VoxReader;
+using VoxReader.Interfaces;
+
+namespace VoxToStructure.format.schematic
+{
+    public class SchematicFile
+    {
+        private Int16 Width;
+        private Int16 Height;
+        private Int16 Length;
+        
+        public NbtFile FromVox(IVoxFile voxFile)
+        {
+            // Access models of .vox file
+            IModel[] models = voxFile.Models;
+            // Access voxels of first model in the file
+            Voxel[] voxels = models[0].Voxels;
+            Vector3 size = models[0].Size;
+            Width = (Int16)size.X;
+            Height = (Int16)size.Y;
+            Length = (Int16)size.Z;
+
+            byte[] blocks = new byte[Width * Height * Length];
+
+            foreach (var voxel in voxels)
+            {
+                // Access properties of a voxel
+                Vector3 position = voxel.Position;
+                //Color color = voxels[0].Color;
+                blocks[BlockIndex(position.X, position.Y, position.Z)] = 1;//all stone for now TODO add blockid based on color
+            }
+            
+            var rootTag = new NbtCompound("root");
+            rootTag.Add(new NbtShort("Width", Width));
+            rootTag.Add(new NbtShort("Height", Height));
+            rootTag.Add(new NbtShort("Length", Length));
+            rootTag.Add(new NbtString("Materials", "Alpha"));//Pocket
+            rootTag.Add(new NbtByteArray("Blocks", blocks));
+            rootTag.Add(new NbtByteArray("Data", new byte[Width * Height * Length]));
+            rootTag.Add(new NbtList("Entities", NbtTagType.Compound));
+            rootTag.Add(new NbtList("TileEntities", NbtTagType.Compound));
+            return new NbtFile(rootTag);
+        }
+
+        public void Save(NbtFile file, string outputPath)
+        {
+            file.SaveToFile(outputPath, NbtCompression.GZip);
+        }
+        
+        private int BlockIndex(int x, int y, int z){
+            return (y * Length + z) * Width + x;
+        }
+    }
+}
